269e4118b4a308809cbd006384f47c8966fb46f3
sweemeng <sweester@gmail.com>
2015-06-29 15:06:13 +0800

    Add missing app config, and db setup

---
+++ app.py
@@ -0,0 +1,11 @@
+__author__ = 'sweemeng'
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import const
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.secret_key = const.secret_key
+app.config["SECRET_KEY"] = const.secret_key
+app.config["SECURITY_POST_LOGOUT_VIEW"] = "/login"
+db = SQLAlchemy(app)
