5b5628c51f25ca11331a45ab532cb45f7c444433
sweemeng <sweester@gmail.com>
2015-07-01 09:32:18 +0800

    Create management script to add user, updated git ignore

---
+++ manage.py
@@ -0,0 +1,21 @@
+__author__ = 'sweemeng'
+from flask.ext.script import Manager
+from flask.ext.security import Security, SQLAlchemyUserDatastore
+from app import db
+from app import app
+from db import User
+from db import Role
+
+manager = Manager(app)
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+@manager.command
+def create_user(email, password):
+    db.create_all()
+    if not user_datastore.find_user(email=email):
+        user_datastore.create_user(email=email, password=password)
+    db.session.commit()
+
+if __name__ == "__main__":
+    manager.run()
